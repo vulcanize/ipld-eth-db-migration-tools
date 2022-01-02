5a435bc0df2ede51ced03cda08d1003bbfe8ce85
i-norden <iansnorden@gmail.com>
2022-01-02 12:58:03 -0600

    refine Writer, Reader, Transformer interfaces; constructor interfaces

--- pkg/interfaces/interfaces.go
+++ pkg/interfaces/interfaces.go
@@ -14,16 +14,30 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-package migration_tools
+package interfaces
 
+import "github.com/jmoiron/sqlx"
+
+// Reader interface for reading v2 DB models for a specific table in a database
 type Reader interface {
-	Read(blockHeights []uint64) ([]interface{}, []uint64, error)
+	Read(blockHeights []uint64) ([][]interface{}, []uint64, error)
 }
 
+// ReaderConstructor func sig for constructing a Reader for a specific table
+type ReaderConstructor func(db *sqlx.DB) Reader
+
+// Writer interface for writing v3 DB models for a specific table in a database
 type Writer interface {
-	Write(models []interface{}) error
+	Write(models [][]interface{}) error
 }
 
+// WriterConstructor func sig for constructing a Writer for a specific table
+type WriterConstructor func(db *sqlx.DB) Writer
+
+// Transformer interface for transforming v2 DB models into v3 DB models for a specific table
 type Transformer interface {
-	Transform(model []interface{}) ([]interface{}, error)
+	Transform(models [][]interface{}) ([][]interface{}, error)
 }
+
+// TransformerConstructor func sig for constructing a Transformer for a specific table
+type TransformerConstructor func() Transformer
